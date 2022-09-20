c14e5625ed0d518c6bca1daffc7de846c18c9e65
DominikS21 <dominik.sabak@live.zs1mm.edu.pl>
2022-09-20 18:55:15 +0200

    Zadanie 4 CSV

--- funkcja.php
+++ funkcja.php
@@ -13,14 +13,14 @@
 </html>
 <?php
 //$content = file_get_contents('http://loripsum.net/api');
-$content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac porttitor turpis, a gravida mi. Curabitur id dolor posuere, scelerisque nibh a, egestas erat. Sed in laoreet enim. Proin in consectetur nisl. Aliquam dapibus, orci id posuere malesuada, augue nisi dapibus elit, fermentum fringilla diam erat ac diam. Donec ut scelerisque ante. Sed nec ex sem. Nulla facilisi. Cras finibus sagittis sapien sed elementum. Integer ut nisi lectus.
+$content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac porttitor turpis, a gravida mi. Curabitur id dolor posuere, scelerisque nibh a, egestas erat. Sed in laoreet enim. Proin in consectetur nisl. Aliquam dapibus, orci id posuere malesuada, augue nisi dapibus elit, fermentum fringilla diam erat ac diam. Donec ut scelerisque ante. Sed nec ex sem. Nulla facilisi. Cras finibus sagittis sapien sed elementum. Integer ut nisi lectus. 
 
 Donec bibendum nisl a orci fermentum, vitae blandit sapien pulvinar. Mauris vitae tellus sit amet ligula tristique facilisis non vel eros. Nunc aliquet ligula leo. Morbi blandit condimentum nibh nec egestas. Maecenas.';
 $array = explode(' ',$content);
 natcasesort($array);
-function renderHTMLTABLE($array) {
-  $find = $_POST['find'];
-  if("" == trim($_POST['find'])){
+
+function renderHTMLTABLE($array, $x, $find) {
+  if("" == trim($find)){
     print_r($array);
   } else {
     foreach($array as $word){
@@ -29,7 +29,6 @@ function renderHTMLTABLE($array) {
       }
     }
     natcasesort($array);
-  $x = $_POST['liczba'];
   echo '<table>';
   $y = $x;
     foreach(array_slice($result, 0, $x) AS $word) {
@@ -49,5 +48,42 @@ function renderHTMLTABLE($array) {
   }
   echo '</table>';
 }
-echo renderHTMLTABLE($array);
+//renderHTMLTABLE($array, $_POST['liczba'], $_POST['find'] );
+function renderCSV($array, $find) {
+  if("" == trim($find)){
+    print_r($array);
+  } else {
+    foreach($array as $word){
+      if( strpos($word, $find) !== false){
+          $result[] = $word;
+      }
+    }
+    natcasesort($array);
+    $file = fopen('php://temp', 'w+');
+    fputcsv($file, $result);
+    rewind($file);
+    while (!feof($file)) {
+    $data_read = fread($file, 8192); // will return a string of all data separeted by commas.
+}
+    fclose($file);
+    echo $data_read;
+}
+
+}
+//renderCSV($array, $_POST['find']);
+function renderMD($array, $x, $find) {
+  if("" == trim($find)){
+    print_r($array);
+  } else {
+    foreach($array as $word){
+      if( strpos($word, $find) !== false){
+          $result[] = $word;
+      }
+    }
+    natcasesort($array);
+}
+
+
+}
+renderMD($array, $_POST['liczba'], $_POST['find']);
 ?>
